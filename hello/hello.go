d40d6354ba56895910a7b07deb785c25f76638c3
Face7or <jacksonleeper.jl@gmail.com>
2023-03-22 19:33:27 -0700

    end handle-errors

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,27 @@
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Face7or/gotut/greetings"
 )
 
 func main() {
-	//fmt.Printf("Hello, GO!")
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings:")
+    log.SetFlags(0)
+
+	// Request a greeting message.
+    message, err := greetings.Hello("")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
